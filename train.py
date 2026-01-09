0c440af457b921172d3e728ed1d250f10a36581a
Naman Omar <myselflearn7@gmail.com>
2026-01-09 21:57:13 +0530

    Model: Lasso, alpha=0.1, no-scaler

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression,Ridge
+from sklearn.linear_model import LinearRegression,Ridge, Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 import os
 
@@ -26,7 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-model = Ridge(alpha=1.0)
+model = Lasso(alpha=0.1)
 model.fit(X_train, y_train)
 
 # Predictions
